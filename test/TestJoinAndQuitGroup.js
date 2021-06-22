f985851bf193c943727f9abe9f4bbd0e3d34054d
zgljl2012 <2693491512@qq.com>
2021-06-22 14:26:43 +0800

    unittest: join group with cpc

--- test/TestJoinAndQuitGroup.js
+++ test/TestJoinAndQuitGroup.js
@@ -8,6 +8,46 @@ contract("GroupChat", (accounts) => {
     await instance.createPublicGroup('test', 'xxxx', 0, '{}')
   })
   it("Join group with 0 cpc", async () => {
-    
+    const instance = await GroupChat.deployed();
+    try {
+      await instance.join(1)
+      assert.fail()
+    } catch(error) {
+      assert.ok(error.toString().includes('You have joinned this group'))
+    }
+    let tx = await instance.join(1, {from: accounts[1]})
+    truffleAssert.eventEmitted(tx, 'JoinGroup', (e) => {
+      assert.equal(e.id, 1)
+      assert.equal(e.addr, accounts[1])
+      return true
+    })
+    assert.equal(await instance.countOf(1), 2, 'The cnt of group is error')
+    assert.equal(await instance.has(1, accounts[1]), true)
+    assert.equal(await instance.has(1, accounts[2]), false)
+  })
+  it('create a group with price', async ()=> {
+    const instance = await GroupChat.deployed();
+    await instance.createPublicGroup('test2', 'xxxx', utils.cpc(1), '{}')
+  })
+  it('join group with cpc', async ()=> {
+    const instance = await GroupChat.deployed();
+    const balance0_1 = await utils.getBalance(accounts[0])
+    try {
+      await instance.join(2, {from: accounts[2]})
+      assert.fail()
+    } catch(error) {
+      assert.ok(error.toString().includes('The value is not equal to the price'))
+    }
+    let tx = await instance.join(2, {from: accounts[2], value: utils.cpc(1)})
+    truffleAssert.eventEmitted(tx, 'JoinGroup', (e) => {
+      assert.equal(e.id, 2)
+      assert.equal(e.addr, accounts[2])
+      return true
+    })
+    assert.equal(await instance.countOf(2), 2, 'The cnt of group is error')
+    assert.equal(await instance.has(2, accounts[2]), true)
+
+    const balance0_2 = await utils.getBalance(accounts[0])
+    assert.equal(utils.add(balance0_1, utils.cpc(1)).toString(), balance0_2, "balance should equal")
   })
 })
--- test/utils.js
+++ test/utils.js
@@ -5,3 +5,11 @@ var BN = web3.utils.BN;
 exports.cpc = (val) => {
   return web3.utils.toWei(String(val), "ether")
 }
+
+exports.getBalance = async (address) => {
+  return await web3.eth.getBalance(address)
+}
+
+exports.add = (a, b) => {
+  return new BN(a).add(new BN(b))
+}
